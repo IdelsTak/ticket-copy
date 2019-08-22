d07cbf2c6adb03541403b576efe7192704c84e6b
Hiram Kamau <hiram.kamau@outlook.com>
2019-08-22 05:31:29 +0300

    Ct.java javadoc

--- src/main/java/com/ct/main/Ct.java
+++ src/main/java/com/ct/main/Ct.java
@@ -1,20 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.ct.main;
 
 import com.ct.controllers.LoginController;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
+ * This is the application's main class/entry point. It is registered in
+ * manifest and used to start the application.
+ * <p>
+ * It shows a GUI by initiating the login process. It achieves this by
+ * instantiating <code>{@link LoginController}</code> and then calling the
+ * controller's <code>displayLogin</code> method.
+ * <p>
+ * This class also sets the look and feel of the entire application. It sets the
+ * look and feel to the system's default. But, in case the application is ran in
+ * a Linux system, it sets the look and feel to Java's cross-platform look.
  *
  * @author admin
  */
 public class Ct {
 
+    /**
+     * The class's logger.
+     */
     private static final Logger LOG = Logger.getLogger(Ct.class.getName());
 
     /**
@@ -25,7 +35,7 @@ public class Ct {
     public static void main(String[] args) {
         //Set the look and feel of the application
         try {
-            String osName = System.getProperty("os.name");
+            var osName = System.getProperty("os.name");
 
             if (osName.equals("Linux")) {
                 // Borders look weird (invisible) on Linux...
@@ -36,8 +46,8 @@ public class Ct {
         } catch (ClassNotFoundException
                  | InstantiationException
                  | IllegalAccessException
-                 | javax.swing.UnsupportedLookAndFeelException ex) {
-            LOG.log(java.util.logging.Level.SEVERE, null, ex);
+                 | UnsupportedLookAndFeelException ex) {
+            LOG.log(Level.SEVERE, null, ex);
         }
 
         //Start the login controller
