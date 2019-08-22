4b63dc78a780ffa9a9a39a2d7643fc0a3f4e2a25
Hiram Kamau <hiram.kamau@outlook.com>
2019-08-22 06:05:01 +0300

    EventModel.java javadoc

--- src/main/java/com/ct/models/EventModel.java
+++ src/main/java/com/ct/models/EventModel.java
@@ -1,8 +1,3 @@
-/*
- * Copyright (c) 2019, Hiram K.
- * All rights reserved.
- *
- */
 package com.ct.models;
 
 import java.math.BigDecimal;
@@ -10,19 +5,70 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 /**
+ * This is essentially a Java <code>{@link Beans}</code> class that represents
+ * the events model. It has seven main properties, namely:
+ * <ul>
+ * <li>eventId
+ * <li>eventType
+ * <li>eventName
+ * <li>venue
+ * <li>dateTime
+ * <li>ticketPrice
+ * <li>remark
+ * </ul>
+ * This class provides the setters as well as the getters for these properties.
  *
  * @author admin
  */
 public class EventModel {
 
+    /**
+     * The id property of the event model.
+     */
     private final String eventId;
+    /**
+     * The type property of the event. It should be only one of these two:
+     * <ul>
+     * <li>Concert
+     * <li>Theater
+     * </ul>
+     */
     private String eventType;
+    /**
+     * The name property of the event.
+     */
     private String eventName;
+    /**
+     * The venue property of the event.
+     */
     private String venue;
+    /**
+     * The date and time property of the event.
+     */
     private LocalDateTime dateTime;
+    /**
+     * The price property of the event.
+     */
     private BigDecimal ticketPrice;
+    /**
+     * The remark property of the event.
+     */
     private String remark;
 
+    /**
+     * Default constructor for an event model. It sets all the properties that
+     * one could expect from an event.
+     *
+     * @param eventId     The id property of the event model.
+     * @param eventType   The type property of the event. It should be only one
+     *                    of these two: (1) <strong>Concert</strong>, or (2)
+     * <strong>Theater</strong>.
+     * @param eventName   The name property of the event.
+     * @param venue       The venue property of the event.
+     * @param dateTime    The date and time property of the event.
+     * @param ticketPrice The price property of the event.
+     * @param remark      The remark property of the event.
+     */
     public EventModel(
             String eventId,
             String eventType,
@@ -40,58 +86,130 @@ public class EventModel {
         this.remark = remark;
     }
 
+    /**
+     * Retrieve the id property of the event model.
+     *
+     * @return the id property.
+     */
     public String getEventId() {
         return eventId;
     }
 
+    /**
+     * Retrieve The type property of the event. It should be only one
+     * of these two: (1) <strong>Concert</strong>, or (2)
+     * <strong>Theater</strong>.
+     *
+     * @return the type property of the event.
+     */
     public String getEventType() {
         return eventType;
     }
 
+    /**
+     * Sets the type property of the event. It should be only one
+     * of these two: (1) <strong>Concert</strong>, or (2)
+     * <strong>Theater</strong>.
+     *
+     * @param eventType the new type property of the event.
+     */
     public void setEventType(String eventType) {
         this.eventType = eventType;
     }
 
+    /**
+     * Retrieve the name property of the event.
+     *
+     * @return the name property of the event.
+     */
     public String getEventName() {
         return eventName;
     }
 
+    /**
+     * Set the name property of the event.
+     *
+     * @param eventName the new name property of the event.
+     */
     public void setEventName(String eventName) {
         this.eventName = eventName;
     }
 
+    /**
+     * Retrieves the venue property of the event.
+     *
+     * @return the venue property of the event.
+     */
     public String getVenue() {
         return venue;
     }
 
+    /**
+     * Sets the venue property of the event.
+     *
+     * @param venue the new venue property of the event.
+     */
     public void setVenue(String venue) {
         this.venue = venue;
     }
 
+    /**
+     * Retrieves the date and time property of the event.
+     *
+     * @return the date and time property of the event.
+     */
     public LocalDateTime getDateTime() {
         return dateTime;
     }
 
+    /**
+     * Sets the date and time property of the event.
+     *
+     * @param dateTime the new date and time property of the event.
+     */
     public void setDateTime(LocalDateTime dateTime) {
         this.dateTime = dateTime;
     }
 
+    /**
+     * Retrieves the price property of the event.
+     *
+     * @return the price property of the event.
+     */
     public BigDecimal getTicketPrice() {
         return ticketPrice;
     }
 
+    /**
+     * Sets the price property of the event.
+     *
+     * @param ticketPrice the new price property of the event.
+     */
     public void setTicketPrice(BigDecimal ticketPrice) {
         this.ticketPrice = ticketPrice;
     }
 
+    /**
+     * Retrieves the remark property of the event.
+     *
+     * @return the remark property of the event.
+     */
     public String getRemark() {
         return remark;
     }
 
+    /**
+     * Sets the remark property of the event.
+     *
+     * @param remark the new remark property of the event.
+     */
     public void setRemark(String remark) {
         this.remark = remark;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int hashCode() {
         int hash = 3;
@@ -99,6 +217,9 @@ public class EventModel {
         return hash;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -114,24 +235,11 @@ public class EventModel {
         return Objects.equals(this.eventId, other.eventId);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
-//        return "EventModel{"
-//                + "eventId="
-//                + eventId
-//                + ", eventType="
-//                + eventType
-//                + ", eventName="
-//                + eventName
-//                + ", venue="
-//                + venue
-//                + ", dateTime="
-//                + dateTime
-//                + ", ticketPrice="
-//                + ticketPrice
-//                + ", remark="
-//                + remark
-//                + '}';
         return eventId + " - " + eventName;
     }
 
