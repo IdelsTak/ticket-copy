d9b6c510e44c4d242dad9c805d4c3b5e36cedd1e
Hiram Kamau <hiram.kamau@outlook.com>
2019-08-22 07:43:39 +0300

    LoginController.java javadoc

--- src/main/java/com/ct/controllers/LoginController.java
+++ src/main/java/com/ct/controllers/LoginController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.ct.controllers;
 
 import com.ct.database.Connection;
@@ -26,22 +21,53 @@ import javax.swing.SwingWorker;
 import static javax.swing.SwingWorker.StateValue.DONE;
 
 /**
+ * A controller class that manages the interactions between the view,
+ * <code>{@link LoginWindow}</code>, and the model,
+ * <code>{@link LoginModel}</code>.
  *
  * @author admin
  */
 public class LoginController {
 
+    /**
+     * The class's logger.
+     */
     private static final Logger LOG = Logger.getLogger(LoginController.class.getName());
-    private final LoginWindow login;
+    /**
+     * The view object.
+     */
+    private final LoginWindow loginWindow;
+    /**
+     * A local cache of the available user names and passwords as provided by
+     * the <code>{@link LoginModel}</code>s.
+     */
     private final Map<Integer, LoginModel> usersCache = new TreeMap<>();
+    /**
+     * The <code>{@link JTextField}</code> retrieved from
+     * <code>{@link LoginWindow}</code> that contains the username.
+     */
     private final JTextField usernameTextField;
+    /**
+     * The <code>{@link JPasswordField}</code> retrieved from
+     * <code>{@link LoginWindow}</code> that contains the password.
+     */
     private final JPasswordField passwordField;
+    /**
+     * The <code>{@link JLabel}</code> retrieved from
+     * <code>{@link LoginWindow}</code> that is used to show sign in errors.
+     */
     private final JLabel statusLabel;
+    /**
+     * A flag value that shows whether a sign in should be allowed.
+     */
     private boolean valid;
 
+    /**
+     * Default constructor for the <code>{@link LoginController}</code>.
+     */
     public LoginController() {
         //Create the login form
-        this.login = new LoginWindow();
+        this.loginWindow = new LoginWindow();
         //Create a swingworker, which will pull data
         //from the database in a background thread
         var loader = new UserLoaders();
@@ -74,14 +100,14 @@ public class LoginController {
 
         //Get the username and password fields from
         //the login form
-        usernameTextField = login.getUsernameTextField();
-        passwordField = login.getPasswordField();
+        usernameTextField = loginWindow.getUsernameTextField();
+        passwordField = loginWindow.getPasswordField();
         //Get the status label where we will
         //display any errors, e.g. incorrect password
-        statusLabel = login.getStatusLabel();
+        statusLabel = loginWindow.getStatusLabel();
         //Get the buttons for login and cancel
-        var loginButton = login.getLoginButton();
-        var cancelButton = login.getCancelButton();
+        var loginButton = loginWindow.getLoginButton();
+        var cancelButton = loginWindow.getCancelButton();
         //Create an action listener that will
         //do something when the login or cancel button
         //is pressed
@@ -99,7 +125,7 @@ public class LoginController {
                 //another window
                 if (valid) {
                     //Close the login window
-                    login.dispose();
+                    loginWindow.dispose();
                     //open the main window
                     var mainWindowController = new MainWindowController();
                     mainWindowController.displayMainWindow();
@@ -118,7 +144,7 @@ public class LoginController {
      */
     public void displayLogin() {
         //Show the login form on the EDT
-        EventQueue.invokeLater(() -> login.setVisible(true));
+        EventQueue.invokeLater(() -> loginWindow.setVisible(true));
     }
     private char[] storedPassword = null;
 
@@ -139,27 +165,32 @@ public class LoginController {
         } else {
             //Start checking whether the password
             //provided by the user is correct
-//            char[] storedPassword = null;
             //Look for the password corresponding to
             //the username in the local cache
+
+            //This is the implementation that uses Java's new
+            //streams and function capabilities.
             usersCache.values()
                     .stream()
                     .filter(val -> username.equals(val.getUsername()))
-                    .forEach(val -> storePassword(val.getPassword()));
-//            for (var loginModel : usersCache.values()) {
-//                if (username.equals(loginModel.getUsername())) {
-//                    //If the username is found in cache
-//                    //then copy the password that is stored
-//                    //for that user in database
-//                    storedPassword = Arrays.copyOf(
-//                            loginModel.getPassword(),
-//                            loginModel.getPassword().length);
-//                    //Since the password has been found
-//                    //we should exit this loop
-//                    break;
-//                }
-//            }
+                    .forEach(val -> setPassword(val.getPassword()));
 
+            //This is the implementation that uses the
+            //old for loop capability
+            /* char[] storedPassword = null;
+             * for (var loginModel : usersCache.values()) {
+             * if (username.equals(loginModel.getUsername())) {
+             * //If the username is found in cache
+             * //then copy the password that is stored
+             * //for that user in database
+             * storedPassword = Arrays.copyOf(
+             * loginModel.getPassword(),
+             * loginModel.getPassword().length);
+             * //Since the password has been found
+             * //we should exit this loop
+             * break;
+             * }
+             * } */
             //If the stored password is equal to
             //what the user has provided, then mark the
             //login as valid
@@ -176,7 +207,12 @@ public class LoginController {
 
     }
 
-    private void storePassword(char[] val) {
+    /**
+     * Sets the password that will be used for signing in.
+     *
+     * @param val the new password that will be used for signing in.
+     */
+    private void setPassword(char[] val) {
         storedPassword = Arrays.copyOf(val, val.length);
     }
 
--- src/main/java/com/ct/views/RoundedDecimal.java
+++ src/main/java/com/ct/views/RoundedDecimal.java
@@ -1,19 +1,22 @@
-/*
- * Copyright (c) 2019, Hiram K.
- * All rights reserved.
- *
- */
 package com.ct.views;
 
 import java.math.BigDecimal;
 import java.util.Objects;
 
 /**
+ * Utility class for displaying numbers formatted to two decimal places.
  *
  * @author admin
  */
 public class RoundedDecimal {
 
+    /**
+     * Displays a number formatted to two decimal places.
+     *
+     * @param val an unformatted number.
+     *
+     * @return a number formatted to two decimal places.
+     */
     public BigDecimal toTwoDecimalPlaces(BigDecimal val) {
         BigDecimal bd = Objects.requireNonNull(val, "Value should not be null");
 
