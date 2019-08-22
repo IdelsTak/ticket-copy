a96198c6c4d115b0c25e4c944162a7e13857ba07
Hiram Kamau <hiram.kamau@outlook.com>
2019-08-22 06:39:01 +0300

    CustomerTableDesign.java javadoc

--- src/main/java/com/ct/views/CustomerTableDesign.java
+++ src/main/java/com/ct/views/CustomerTableDesign.java
@@ -1,13 +1,16 @@
-/*
- * Copyright (c) 2019, Hiram K.
- * All rights reserved.
- *
- */
 package com.ct.views;
 
+import com.ct.models.CustomerModel;
 import javax.swing.table.DefaultTableModel;
 
 /**
+ * This is the <code>{@link DefaultTableModel}</code> of a table containing
+ * <code>{@link CustomerModel}</code> properties.
+ * <p>
+ * It defines the columns and also adds support for showing
+ * <code>{@link Boolean}</code> values as check boxes.
+ *
+ * @see #getColumnClass(int columnIndex)
  *
  * @author admin
  */
@@ -26,12 +29,28 @@ public class CustomerTableDesign extends DefaultTableModel {
         "Issued"
     };
 
+    /**
+     * Default constructor for the <code>{@link CustomerTableDesign}</code>,
+     * whose main function is to define the column names.
+     */
     public CustomerTableDesign() {
         for (String columnName : COLUMN_NAMES) {
             super.addColumn(columnName);
         }
     }
 
+    /**
+     * Describes the class types for the customers table. It overrides the
+     * parent class's method to avoid returning only <code>{@link Object}</code>
+     * for all columns.
+     * <p>
+     * The approach allows the return of <code>{@link Boolean}</code> class in
+     * columns 7 and 8, which in turn force the table to paint check boxes.
+     *
+     * @param columnIndex the column index to determine the class type for.
+     *
+     * @return the class type for a given column index.
+     */
     @Override
     public Class<?> getColumnClass(int columnIndex) {
         switch (columnIndex) {
