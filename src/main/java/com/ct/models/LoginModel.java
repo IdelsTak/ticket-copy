91a50f108271b8643004c6531a34ac0587b0299b
Hiram Kamau <hiram.kamau@outlook.com>
2019-08-22 05:48:51 +0300

    LoginModel.java javadoc

--- src/main/java/com/ct/models/LoginModel.java
+++ src/main/java/com/ct/models/LoginModel.java
@@ -1,21 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.ct.models;
 
+import com.ct.controllers.LoginController;
+import java.beans.Beans;
 import java.util.Arrays;
-import java.util.logging.Logger;
 
 /**
+ * This is essentially a Java <code>{@link Beans}</code> class that represents
+ * the login model. It has two main properties, namely:
+ * <ul>
+ * <li>username
+ * <li>password
+ * </ul>
+ * The <code>{@link LoginController}</code> calls these properties to help it to
+ * sign in to the application.
  *
  * @author admin
  */
 public class LoginModel {
 
-    private static final Logger LOG = Logger.getLogger(LoginModel.class.getName());
+    /**
+     * The username property that will be used to sign in to the application.
+     */
     private final String username;
+    /**
+     * The password property that will be used to sign in to the application.
+     */
     private final char[] password;
 
     /**
@@ -29,15 +38,30 @@ public class LoginModel {
         this.password = password;
     }
 
+    /**
+     * Retrieves the username property to sign in with.
+     *
+     * @return the username property.
+     */
     public String getUsername() {
-
         return username;
     }
 
+    /**
+     * Retrieves the password property to sign in with. Out of best practice,
+     * the method provides a defensive copy of the password <code>char</code>s
+     * to avoid mutating the internals of this class.
+     *
+     * @return the password property.
+     */
     public char[] getPassword() {
-        return password;
+        return Arrays.copyOf(password, password.length);
     }
 
+    /**
+     *
+     * {@inheritDoc}
+     */
     @Override
     public String toString() {
         return "LoginModel{"
